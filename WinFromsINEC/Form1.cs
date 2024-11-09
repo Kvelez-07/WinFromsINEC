b7abc353fc5ea779ac8615920958c4f40fb2abc3
Kevin <kvelezsalazar07@gmail.com>
2024-11-08 19:43:04 -0600

    Comment Additions, v1.2

--- WinFromsINEC/Form1.cs
+++ WinFromsINEC/Form1.cs
@@ -12,18 +12,21 @@ using System.Windows.Forms;
 
 namespace WinFromsINEC
 {
-    public partial class frmINEC : Form
+    public partial class frmINEC : Form // WinForms 4.8.1
     {
         #region Attributes
-        static PopulationAge[] Age_Arr = new PopulationAge[40];
+        // Los arreglos que se van a utilizar para almacenar la información de las personas
+        // Esto no remplaza una base de datos, pero tiene el problema de hacer lectura secuenciaoles.
+        // El archivo no viene sumarizado, por lo cual se deben hacer distinciones de 0-4, 5-9, 10-14, 15-19, 20-49 (a partir de los 20 si esta sumarizado de 20-49), 50-59, 60+
+        static PopulationAge[] Age_Arr = new PopulationAge[40]; // Se fijan 40, a pesar de que no se sabe cuantas edades hay, pero se fija un número para que no haya problemas
         static int[] Male_Arr = { 0, 0, 0, 0 }; // Resumen de los grupos de personas acomodados por su edad, siendo estos hombres
         static int[] Female_Arr = { 0, 0, 0, 0 }; // Resumen de los grupos de personas acomodados por su edad, siendo estas mujeres
         static int[] Scholarship_Arr = { 0, 0, 0, 0, 0, 0, 0 }; // Resumen del total de personas por cada nivel deeducación
 
         // Todas los niveles de educación en un arreglos de strings
-        static string[] Scholarship_Label_Arr =
+        static string[] Scholarship_Label_Arr = // Posiciones 16 en adelante en el ASCII.txt
         {
-            "Primaria Completa",
+            "Primaria Completa", // Headings o Rotulos
             "Primaria Incompleta",
             "Secundaria Completa",
             "Secundaria Incompleta",
@@ -41,9 +44,9 @@ namespace WinFromsINEC
         //Instancia de las utilidades
         UtilitiesINEC Census_Utils = new UtilitiesINEC();
 
-        Thread Male_Age_Thread; // Hilo para generar los datos de los hombres
+        Thread Male_Age_Thread; // Hilo para generar los datos (grupo etario) de los hombres
         Thread Female_Age_Thread; // Hilo para generar los datos de las muejeres
-        Thread Scholarship_Thread; // Hilo para generar los datos de la escolaridad
+        Thread Scholarship_Thread; // Hilo para generar los datos de la escolaridad/educación
         #endregion
 
         public frmINEC()
@@ -56,7 +59,7 @@ namespace WinFromsINEC
         private void btnLoadFile_Click(object sender, EventArgs e)
         {
             //Evento que sucede al darle click al botón de "Cargar archivo"
-            //Del archivo de texto obtiene la información
+            //Del archivo de texto obtiene la información y pasa al DataGrid
             ReadFile();
 
             //Se llena el data grid view con la información del archivo plano
@@ -71,9 +74,9 @@ namespace WinFromsINEC
             ActivateProgressBar(true, "Calculando", 1, 3, 1);
 
             //Checkea que los hilos no estén siendo ejecutados y en caso de que no sea así los ejecuta
-            if (!Male_Age_Thread.IsAlive)
-                Male_Age_Thread.Start();
-            //EN caso de que esté ya ejecutandose envía un mensaje para indicar esto
+            if (!Male_Age_Thread.IsAlive) // Si el hilo no está corriendo
+                Male_Age_Thread.Start(); // Se inicia el hilo
+            //En caso de que esté ya ejecutandose envía un mensaje para indicar esto
             else Census_Utils.MessageThread(Male_Age_Thread);
 
             if (!Female_Age_Thread.IsAlive)
@@ -86,12 +89,12 @@ namespace WinFromsINEC
             else
                 Census_Utils.MessageThread(Scholarship_Thread);
 
-            //Se invoca la acción dentro del parentesis para que se ejecute por aparte y otros procesos no agarren su espacio
-            gridAgeToGender.Invoke(new Action(() =>
+            //Se invoca la acción dentro del parentesis para que se ejecute por aparte y otros procesos no agarren espacios ocupados.
+            gridAgeToGender.Invoke(new Action(() => // Inoke hace una proteccion para multiprogramacion, donde los elementos de R/W (escritura) deben ser protegidos.
             {
-                while (Male_Age_Thread.ThreadState == ThreadState.Running)
+                while (Male_Age_Thread.ThreadState == ThreadState.Running) // Si el hilo esta corriendo debe esperar
                 {
-                    Thread.Sleep(1000);
+                    Thread.Sleep(1000); // Libreria de hilos y espera
                 }
 
                 while (Female_Age_Thread.ThreadState == ThreadState.Running)
@@ -99,13 +102,16 @@ namespace WinFromsINEC
                     Thread.Sleep(1000);
                 }
 
+                // El tercer hilo no se contemplo, pero se puede agregar
+                // De momento se infiere que si estos son los unicos que estan esperando, entonces el que resta esta trabajando.
+
                 //Se envia un mensaje para que se sepa que ya se terminó de ejecutar el hilo
                 Census_Utils.LogMessage("Thread execution finished");
                 //Coloca los valores del datagridview en 0
                 ActivateProgressBar(false, string.Empty, 0, 0, 0);
                 //Llena los DGV de genero y de escolaridad
-                FillGenderDataGrid();
-                FillScholarshipDataGrid();
+                FillGenderDataGrid(); // DataGrid Secundario de Edades y Genero
+                FillScholarshipDataGrid(); // DataGrid de Escolaridad
 
             }));
         }
@@ -157,9 +163,17 @@ namespace WinFromsINEC
         #region Extracción de datos del archivo plano
         public void ReadFile()
         {
+            // El archivo plano tipo ASCII que no esta delimitado por comas sino por longitudes.
+            // Se debe trabajar por posiciones y por ende se trabaj con arrays/arreglos.
+            // Las dos primeras son las edades
+            // Las otras 7 son la proyeccion de cantidad de hombres
+            // Las otras 7 es la proyeccion de cantidad de mujeres
+            // De la posicion 16 en adelante cada 6 corresponde a un grupo educacional.
+            // El ASCII no requiere de un programa extre para verse -> C# StreamReader
+
             //Se obtiene la ruta rerlativa del archivo plano
-            string fileName = @".\DB\Proyeccion_2025.txt";
-            //Se almacena el largo del archivo
+            string fileName = @".\DB\Proyeccion_2025.txt"; // Los datos estan basados en porcentajes, puede que haya diferencias de 2 a 3 personas.
+            //Se almacena la longitud del archivo, eso permite al progress bar mostrar correctamente como va avanzando
             int length = Convert.ToInt32(new FileInfo(fileName).Length);
             //Varieble contador
             int step = 0;
@@ -170,24 +184,24 @@ namespace WinFromsINEC
             {
                 //Se lee la información del archivo de texto
                 var reader = new StreamReader(fileName);
-                //Lee la primer linea del archivo de texto
+                //Lee la primer linea del archivo de texto para verificar que tenga contenido
                 line = reader.ReadLine();
                 //Coloca la variable contador como el valor total del largo de esa linea que se obtuvo
-                step = Convert.ToInt32(line.Length);
+                step = Convert.ToInt32(line.Length); // Cada vez que lee un registro, se obtiene la longitud de la linea para que el progress bar camine
 
                 //Activa la barra de progreso
                 ActivateProgressBar(true, "Reading File", 1, length, step);
                 //Se reinicia el valor de la progres var
                 prgAge.Refresh();
 
-                //Mientras haya algun valor en la liena
-                while (line != null)
+                //Mientras no sea el final del archivo
+                while (line != null) // Mientras haya lineas en el archivo
                 {
                     //Obtiene los datos que se encuentran en la linea
                     GetFileData(line);
                     //Lee la siguiente liena del archivo planoi
                     line = reader.ReadLine();
-                    //Avanza unnpaso en la barra de progreso
+                    //Avanza la barra de progreso
                     prgAge.PerformStep();
                 }
                 //Cierra el lector del archivo plano y libera los recursos del sistema
@@ -196,7 +210,7 @@ namespace WinFromsINEC
                 prgAge.Value = length;
                 //Se envia el mensaje de exito del proceso
                 Census_Utils.LogMessage("File read successfully");
-                //Se coulat la progress bar
+                //Se borra la barra para seguir trabajando
                 ActivateProgressBar(false, string.Empty, 0, 0, 0);
             }
             catch (Exception ex)//Si hay algún error lo muestra en la consola
@@ -207,7 +221,7 @@ namespace WinFromsINEC
 
         public void GetFileData(string fileLine)
         {
-            //Se inicializa la variable del arreglo para almacenar los datos
+            //Instancia de la clase edad y lee de acuerdo a las posiciones
             Age_Arr[Age_Index] = new PopulationAge();
 
             //Estos sub string funcionan de manera que se coloca el indice desde el que se comienza a leer
@@ -244,13 +258,13 @@ namespace WinFromsINEC
             Age_Arr[Age_Index].Scholarship[6] = Convert.ToInt32(fileLine.Substring(52, 6));
 
             //Lo que tiene almacenado en el Age_Arr[age_index] es el total de los hombres que había en esta linea
-            Male_Total += Age_Arr[Age_Index].Male;
+            Male_Total += Age_Arr[Age_Index].Male; // Acumulador de totales de hombres
 
             //Lo que tiene almacenado en el Age_Arr[age_index] es el total de las muejeres que había en esta linea
-            Female_Total += Age_Arr[Age_Index].Female;
+            Female_Total += Age_Arr[Age_Index].Female; // Acumulador de totales de mujeres
 
             //Se imprimen los valores obtenidos en la consola
-            Console.WriteLine($"**** {Age_Arr[Age_Index].Age} ****");
+            Console.WriteLine($"**** {Age_Arr[Age_Index].Age} ****"); // Bitacora en la ventana de salida / output
             Console.WriteLine(Age_Arr[Age_Index].Scholarship[0]);
             Console.WriteLine(Age_Arr[Age_Index].Scholarship[1]);
             Console.WriteLine(Age_Arr[Age_Index].Scholarship[2]);
@@ -279,10 +293,10 @@ namespace WinFromsINEC
             //Ciclo for para el calculo
             for (int i = 0; i < Age_Arr.Length; i++)
             {
-                //SI lo que está en la posición actual del arreglo no es nulo
+                //Si lo que está en la posición actual del arreglo no es nulo
                 if (Age_Arr[i] != null)
                 {
-                    // COn el lock y el .syncRoot se asegura de que solo 1 hilo pueda estar modificando los valores en el interior del arreglo
+                    // Con el lock y el .syncRoot se asegura de que solo 1 hilo pueda estar modificando los valores en el interior del arreglo
                     lock (Male_Arr.SyncRoot)
                     {
                         
@@ -300,7 +314,7 @@ namespace WinFromsINEC
             }
 
             //Imprime los valores obtenidos en la consola
-            Console.WriteLine("Male");
+            Console.WriteLine("Male"); // Bitacora en la ventana de salida / output (backend)
             Console.WriteLine(Male_Arr[0]);
             Console.WriteLine(Male_Arr[1]);
             Console.WriteLine(Male_Arr[2]);
@@ -316,7 +330,6 @@ namespace WinFromsINEC
         public void CalculateWomen()
         {
             // Por el uso del invoke se protege al hilo para indicar con el que se está trabajando
-
             txtThreadId.Invoke(new Action(() =>
             {
                 //Se coloca en el textbox el nombre del hilo que se esta manejando
@@ -358,12 +371,12 @@ namespace WinFromsINEC
         }
         public void CalculateScholarship()
         {
-            txtThreadId.Invoke(new Action(() =>
+            txtThreadId.Invoke(new Action(() => // Se coloca en el textbox el nombre del hilo que se esta manejando
             {
                 txtThreadId.Text = Scholarship_Thread.ManagedThreadId.ToString();
             }));
 
-            for (int i = 0; i < Age_Arr.Length; i++)
+            for (int i = 0; i < Age_Arr.Length; i++) // Obtiene longitud del vector de edades
             {
                 if (Age_Arr[i] != null)
                 {
@@ -390,23 +403,21 @@ namespace WinFromsINEC
         public void AddScholarship(int index, int age_index)
         {
             //A partir de contador del ciclo for y el dontador de la posción en el arreglo de escolaridad
-            for (int i = 0; i < Age_Arr[age_index].Scholarship.Length; i++)
-                //Va aumentando el total
-                Scholarship_Arr[index] += Age_Arr[age_index].Scholarship[i];
+            for (int i = 0; i < Age_Arr[age_index].Scholarship.Length; i++) // Un vector de 7 posiciones por grupo etario
+                Scholarship_Arr[index] += Age_Arr[age_index].Scholarship[i]; // Totales del acumulador
         }
 
         #endregion
 
-
         #region carga datos en los gridview
         public void FillPeopleDataGrid()
         {
-            gridAgeToGender.Font = new Font("Arial", 10);
-            gridAgeToGender.Columns.Add("Age", "Age");
+            gridAgeToGender.Font = new Font("Arial", 10); // Manipulacion de DataGrid - FonFamily
+            gridAgeToGender.Columns.Add("Age", "Age"); // Columnas a mostrar
             gridAgeToGender.Columns.Add("Male", "Male");
             gridAgeToGender.Columns.Add("Female", "Female");
 
-            gridAgeToGender.Columns["Male"].DefaultCellStyle.Format = "#.##";
+            gridAgeToGender.Columns["Male"].DefaultCellStyle.Format = "#.##"; // Formato de los datos
             gridAgeToGender.Columns["Female"].DefaultCellStyle.Format = "#.##";
 
             gridAgeToGender.Columns["Male"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
@@ -416,15 +427,15 @@ namespace WinFromsINEC
             gridAgeToGender.Columns["Male"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             gridAgeToGender.Columns["Female"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
 
-            for (int i = 0; i < Age_Arr.Length; i++)
+            for (int i = 0; i < Age_Arr.Length; i++) // Recorre el vector de edades
             {
-                if (Age_Arr[i] != null)
+                if (Age_Arr[i] != null) // Si no es nulo
                 {
-                    gridAgeToGender.Rows.Add(Age_Arr[i].Age, Age_Arr[i].Male, Age_Arr[i].Female);
+                    gridAgeToGender.Rows.Add(Age_Arr[i].Age, Age_Arr[i].Male, Age_Arr[i].Female); // Agrega los datos al DataGrid
                 }
             }
 
-            gridAgeToGender.Refresh();
+            gridAgeToGender.Refresh(); // El primer refresh es muy basico, solo requiere trasladar los datos
         }
 
         public void WriteSubTotal(string heading, int maleTotal, int femaleTotal, bool status, int type)
@@ -486,10 +497,9 @@ namespace WinFromsINEC
                     //Conmvierte la edad en un string
                     string ageStr = Age_Arr[i].Age.ToString();
                     //Si el contador es igual a 0 en la columna "Total" coloca los totales completos de hombres, de mujeres y el general
-                    if (i == 0)
+                    if (i == 0) // Si esta en el indice primario debe dar el total
                         WriteSubTotal("Total", Male_Total, Female_Total, false, 1);
 
-                    
                     if (Age_Arr[i].Age == 0)//Rango de 0 - 4
                         WriteSubTotal("0-4", Male_Arr[0], Female_Arr[0], false, 1);//Aquí ya estan almacenados lo totales de cada rango por lo que se añaden los datos
                     else if (Age_Arr[i].Age == 5) //Rango de 5 - 9
@@ -568,7 +578,7 @@ namespace WinFromsINEC
                     else if (Age_Arr[i].Age == 60)//Rango de 60+
                         WriteSubTotal("60+", Scholarship_Arr[6], 0, true, 2); //Se agrega una linea antes de este y se coloca la información
 
-                    for (int j = 0; j < Age_Arr[i].Scholarship.Length; j++)
+                    for (int j = 0; j < Age_Arr[i].Scholarship.Length; j++) // Un vector de 7 posiciones por grupo etario
                     {
                         //Se añaden los valores al data grid view de ecolaridad
                         gridScholarship.Rows.Add(Age_Arr[i].Age, Scholarship_Label_Arr[j], Age_Arr[i].Scholarship[j]);
